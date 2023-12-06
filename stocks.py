254a3945a32895a56250fe8c31a69e63c52a3304
Mirap9615 <130103615+Mirap9615@users.noreply.github.com>
2023-12-06 00:00:22 -0500

    Stock Predictions
    Uses average of linear and SMA with a sliding window approach to forecast stock prices for a future of x days

--- stocks.py
+++ stocks.py
@@ -3,6 +3,8 @@ from datetime import datetime, timedelta
 import pandas as pd
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 
 def fetch_stock_data(ticker, start_date, end_date, interval='1d'):
@@ -114,35 +116,73 @@ def stock_anomalies_detection(user_stock, user_chosen_start_date):
     display_anomalies(anomalies_detected)
 
 
-def get():
-    """
-    end_date = datetime.now()
-    start_date = end_date - timedelta(days=7)
-    formatted_start_date = start_date.strftime('%Y-%m-%d')
-    formatted_end_date = end_date.strftime('%Y-%m-%d')
+def calculate_sma(data, window):
+    """Calculate Simple Moving Average for a specified window."""
+    return data.rolling(window=window).mean()
 
-    user_stock_data = fetch_stock_data(user_stock, formatted_start_date, formatted_end_date, interval='1d')
-    sp500_data = fetch_stock_data('^GSPC', formatted_start_date, formatted_end_date, interval='1d')
-    print(user_stock_data)
-    print(sp500_data)
-    if base_filter(user_stock_data, sp500_data, user_stock) and learning_center(user_stock, datetime.now()):
-        # Double-confirmed to not follow the market trend
-        pass
-    else:
-        # Follows the market trend
-        pass
-    :return:
-    """
+
+def linear_regression_forecast(data, lags):
+    """Forecast using Linear Regression with specified lags."""
+    X = np.column_stack([data.shift(i) for i in range(1, lags + 1)])
+    X = X[~np.isnan(X).any(axis=1)]
+    y = data[len(data) - len(X):]
+
+    model = LinearRegression()
+    model.fit(X, y)
+    return model.predict([X[-1]])[0]
+
+
+def ensemble_forecast(data, lags, window, forecast_horizon):
+    """Ensemble forecast using Linear Regression and SMA."""
+    forecasts = []
+
+    for _ in range(forecast_horizon):
+        # Linear Regression Forecast
+        lr_forecast = linear_regression_forecast(data, lags)
+
+        # SMA Forecast
+        sma_forecast = calculate_sma(data, window).iloc[-1]
+
+        # Ensemble (Taking the average of LR and SMA forecasts)
+        ensemble_forecast = (lr_forecast + sma_forecast) / 2
+        forecasts.append(ensemble_forecast)
+
+        # Append the ensemble forecast to the data for the next iteration
+        data = data._append(pd.Series(ensemble_forecast), ignore_index=True)
+
+    return forecasts
+
+
+def display_forecast(forecasts, start_date, forecast_duration):
+    forecast_dates = [start_date + timedelta(days=i) for i in range(forecast_duration)]
+    for date, forecast in zip(forecast_dates, forecasts):
+        print(f"Date: {date.strftime('%Y-%m-%d')}, Predicted Close: {forecast:.2f}")
+
+
+def stock_levels_prediction(user_stock, user_chosen_start_date, forecast_duration):
+    # Not using SARIMA, using combination linear and Simple Moving Average
+    # Fetch and preprocess data
+    start_date = user_chosen_start_date - timedelta(days=365)
+    end_date = user_chosen_start_date - timedelta(days=1)
+
+    lags = 5  # Number of lagged features for Linear Regression
+    window = 5  # Window size for SMA
+    data = fetch_stock_data(user_stock, start_date, end_date, interval='1d')['Close']
+    forecasts = ensemble_forecast(data, lags, window, forecast_duration)
+
+    display_forecast(forecasts, user_chosen_start_date, forecast_duration)
+
+    return forecasts
     pass
 
 
 def main():
-    # 1. Data Changes
-    # 2. Anomaly Detection
-    # 3. Model Training and Evaluation
+    # User Stock
     user_stock = 'STLA' # input("Enter the stock ticker: ")
-
+    # Anomaly Detection
     stock_anomalies_detection(user_stock, datetime.now())
+    # Stock Prediction
+    stock_levels_prediction(user_stock, datetime.now(), 5)
 
 
 if __name__ == "__main__":
